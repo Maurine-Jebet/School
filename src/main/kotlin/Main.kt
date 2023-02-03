abe43d347cad28ca4bbacb88b817ecc98590844b
Maurine-Jebet <maurinejebett@gmai.com>
2023-02-03 07:16:17 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,13 @@
+fun main() {
+    var name = "Maurine-Jebet"
+    println(name)
+    var age: Byte =20
+    println(age)
+    var phonenumber = "0714788665"
+    println(phonenumber)
+    val weight = "44"
+    print(weight)
+    val CitizenKenyan = "true"
+    println(CitizenKenyan)
+
+}
